52149edf3fbf4d229dbf63acecaadbfca0492ccd
01KingMaker <fabrakoto0987@gmail.com>
2023-11-13 09:30:27 +0300

    script

---
+++ Clustering/build.sh
@@ -0,0 +1,25 @@
+lib=lib
+
+find -name '*.java' > src.txt
+
+javac -cp "$lib/*" -parameters -d temporary/WEB-INF/classes @src.txt
+rm src.txt
+
+if [ -f Jsp/css ]
+then
+cp  Jsp/css ./temporary
+fi
+
+cp -r Jsp/ ./temporary
+
+cp web.xml temporary/WEB-INF
+
+mkdir temporary/WEB-INF/lib
+
+cp -r $lib temporary/WEB-INF/
+
+cd temporary then
+
+jar -cf ../Project.war .
+cd ..
+rm -r temporary
--- Clustering/src/java/HelloServlet.java
+++ Clustering/src/java/HelloServlet.java
@@ -6,7 +6,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author fabien
  */
-@WebServlet(urlPatterns = {"/"})
 public class HelloServlet extends HttpServlet {
 
     /**
@@ -29,7 +27,7 @@ public class HelloServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8"); 
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
